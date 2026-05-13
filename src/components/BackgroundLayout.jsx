198f461abc2f8bf0be391b8c5bfd3c8e5ba59f81
abonnur04-svg <abonnur04@gmail.com>
2026-05-13 23:17:06 +0500

    Update BackgroundLayout.jsx

--- src/components/BackgroundLayout.jsx
+++ src/components/BackgroundLayout.jsx
@@ -1,4 +1,4 @@
-const BG_IMAGE = "https://media.base44.com/images/public/69d21c4242b48b94ff5afa5f/2455fd508_generated_image.png";
+const BG_IMAGE = "https://www.caravan.kz/wp-content/uploads/2025/05/6ec4fecc-7e61-4dfe-8c50-288d1c5c2ed8-1024x683.png";
 
 export default function BackgroundLayout({ children, overlayOpacity = "bg-black/60" }) {
   return (
